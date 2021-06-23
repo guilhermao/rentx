031ed61a8a39ffda4080b88dba65c3d8ca03ce7b
unknown <22guigs@gmail.com>
2021-06-23 11:23:34 -0400

    chore: add simple email service feature

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,28 +1,3 @@
-import { container } from "tsyringe";
-
-import { IDateProvider } from "./dateprovider/IDateProvider";
-import { DayjsDateProvider } from "./dateprovider/implementations/DayjsDateProvider";
-import { IMailProvider } from "./mailprovider/IMailProvider";
-import { EtherealMailProvider } from "./mailprovider/implementations/EtherealMailProvider";
-import { LocalStorageProvider } from "./storageprovider/implementations/LocalStorageProvider";
-import { S3StorageProvider } from "./storageprovider/implementations/S3StorageProvider";
-import { IStorageProvider } from "./storageprovider/IStorageProvider";
-
-container.registerSingleton<IDateProvider>(
-  "DayjsDateProvider",
-  DayjsDateProvider
-);
-
-container.registerInstance<IMailProvider>(
-  "EtherealMailProvider",
-  new EtherealMailProvider()
-);
-
-const diskStorage = {
-  local: LocalStorageProvider,
-  s3: S3StorageProvider,
-};
-container.registerSingleton<IStorageProvider>(
-  "StorageProvider",
-  diskStorage[process.env.disk]
-);
+import "./dateprovider";
+import "./mailprovider";
+import "./storageprovider";
